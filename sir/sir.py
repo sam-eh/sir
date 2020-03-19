1f8ccea6c0f6b2aa5a050e72933bb2a0516e85da
spmay523 <spmay523@gmail.com>
2020-03-18 22:16:21 -0400

    simulation adjusted to be done in 5 day increments to demonstrate change over time

--- sir/sir.py
+++ sir/sir.py
@@ -8,40 +8,50 @@ from population import Population
 #        simulate those infected showing symptoms and quarantining themselves
 #        simulate infecting of pop by active infected persons
 #        report out the SIR numbers
-
-days = 30
 day = 1
-# just running this for a single population; eventually could benefit from simulating populations where individuals travel between
 my_pop = Population()
 # choose 3 people to infect initially
 initial_infect = my_pop.get_susceptiblePersons()[0:2]
 for person in initial_infect:
     person.infected(day)
-day_results = []
-day_results.append(my_pop.get_numTypes())
-while day <= days:
-    print(f"-----\nday: {day}")
-    my_pop.infect_pop(day)
-    my_pop.quarantine_pop(day)
-    my_pop.recover_pop(day)
-    day_results.append(my_pop.get_numTypes())
-    # print(f"\n{my_pop}")
-    day += 1
 
-susceptible, infected, recovered = [], [], []
-for result in day_results:
-    susceptible.append(result["s"])
-    infected.append(result["i"])
-    recovered.append(result["r"])
+day_results = {"s":[], "i":[], "r":[]}
+result = my_pop.get_numTypes()
+day_results["s"].append(result["s"])
+day_results["i"].append(result["i"])
+day_results["r"].append(result["r"])
 
-days_list = [i for i in range(0,days+1)]
+days_list = [0]
+day_countdown = 5
 
-fig, ax = plt.subplots(figsize=(14,7))
-s = ax.scatter(days_list, susceptible, c="blue")
-i = ax.scatter(days_list, infected, c="red")
-r = ax.scatter(days_list, recovered, c="green")
-ax.set_title(f"Simulation of Population Infection/Recovery Over {days} Days")
-ax.get_xaxis().set_label("Day")
-ax.get_yaxis().set_label("Population Count")
-ax.legend((s, i, r), ('Susceptible', 'Infected', 'Recovered'))
-plt.show()
+while True:
+    while day_countdown > 0:
+        print(f"-----\nday: {day}")
+        my_pop.infect_pop(day)
+        my_pop.quarantine_pop(day)
+        my_pop.recover_pop(day)
+        result = my_pop.get_numTypes()
+        day_results["s"].append(result["s"])
+        day_results["i"].append(result["i"])
+        day_results["r"].append(result["r"])
+        days_list.append(day)
+        day += 1
+        day_countdown -= 1
+
+    # plot current item
+    fig, ax = plt.subplots(figsize=(14,7))
+    s = ax.scatter(days_list, day_results["s"], c="blue")
+    i = ax.scatter(days_list, day_results["i"], c="red")
+    r = ax.scatter(days_list, day_results["r"], c="green")
+    ax.set_title(f"Simulation of Population Infection/Recovery Over {day} Days")
+    ax.get_xaxis().set_label("Day")
+    ax.get_yaxis().set_label("Population Count")
+    ax.legend((s, i, r), ('Susceptible', 'Infected', 'Recovered'))
+    plt.show()
+
+    # assess if should continue with simulation
+    keep_going = input("Continue with 5 next days of simulation? y/n\n")
+    if keep_going == "n":
+        break;
+    else:
+        day_countdown = 5
